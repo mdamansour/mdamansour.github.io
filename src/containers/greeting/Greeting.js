9fda10e9dc7d9bd3d304911727e193be8806cf53
WINDOWS\simo <md@grylab.com>
2025-09-25 17:00:07 +0100

    fixed donload resume again from link in greetings

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={require("https://md.amansour.me/Resume/Resume.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
